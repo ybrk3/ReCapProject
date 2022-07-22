883e0b199570aaf44ab38167cde4572667a86dc7
byildirim <byildirim@ISTLKGNI19>
2022-07-22 16:59:19 +0300

    Add project files.

---
+++ ConsoleUI/Program.cs
@@ -0,0 +1,11 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Business.Concrete;
+using DataAccess.Concrete;
+
+CarManager carManager = new CarManager(new InMemoryCar());
+foreach (var car in carManager.GetById(2))
+{
+    Console.WriteLine(car.Description);
+}
+
+
---
+++ DataAccess/Concrete/InMemoryCar.cs
@@ -0,0 +1,57 @@
+﻿using DataAccess.Abstract;
+using Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess.Concrete
+{
+    public class InMemoryCar : ICarDal
+    {
+        List<Car> _cars;
+        public InMemoryCar()
+        {
+            _cars = new List<Car> {
+                new Car { Id=1, BrandId=1, ModelYear="2021", DailyPrice=325,Description="Audi A4, diesel, dijital klima, hız sabitleme, şerit takip vardır." },
+                new Car { Id=2, BrandId=1, ModelYear="2022", DailyPrice=450,Description="Audi A3, diesel, dijital klima, hız sabitleme, şerit takip vardır." },
+                new Car { Id=3, BrandId=2, ModelYear="2021", DailyPrice=250,Description="Ford Focus, diesel, dijital klima, hız sabitleme, şerit takip vardır." },
+                new Car { Id=4, BrandId=2, ModelYear="2021", DailyPrice=275,Description="Ford Fiesta, diesel, dijital klima, hız sabitleme, şerit takip vardır." },
+                new Car { Id=5, BrandId=3, ModelYear="2022", DailyPrice=2150,Description="Fiat Linea, diesel, dijital klima, hız sabitleme, şerit takip vardır." }
+            };
+        }
+        public void Add(Car car)
+        {
+            _cars.Add(car);
+        }
+        public void Update(Car car)
+        {
+            Car carToUpdate = _cars.FirstOrDefault(c => c.Id == car.Id);
+            carToUpdate.Id = car.Id;
+            carToUpdate.BrandId = car.BrandId;
+            carToUpdate.ModelYear = car.ModelYear;
+            carToUpdate.DailyPrice = car.DailyPrice;
+            carToUpdate.Description = car.Description;
+        }
+
+        public void Delete(Car car)
+        {
+            Car carToDelete = null;
+            carToDelete = _cars.FirstOrDefault(c => c.Id == car.Id);
+            _cars.Remove(carToDelete);
+        }
+
+        public List<Car> GetAll()
+        {
+            return _cars;
+        }
+
+        public List<Car> GetById(int brandId)
+        {
+            return _cars.Where(c => c.BrandId == brandId).ToList();
+        }
+
+
+    }
+}
